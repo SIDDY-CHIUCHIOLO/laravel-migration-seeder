0215408656e5a26bed847c596743f26ae23f9b89
siddhartha chiuchiolo <siddychiuchiolo7@gmail.com>
2022-05-16 16:44:17 +0200

    aggiunto seeder di train

---
+++ database/seeds/TrainsTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Train;
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i=0; $i < 50; $i++) { 
+            $newTrain = new Train();
+            $newTrain->Azienda = $faker->name();
+            $newTrain->Stazione_di_partenza = $faker->city();
+            $newTrain->Stazione_di_arrivo = $faker->city();
+            $newTrain->Data_di_partenza = $faker->date();
+            $newTrain->Data_di_arrivo = $faker->date();
+            $newTrain->Orario_di_partenza = $faker->time();
+            $newTrain->Orario_di_arrivo = $faker->time();
+            $newTrain->Codice_Treno = $faker->numberBetween(1000, 20000);
+            $newTrain->Numero_Carrozze = $faker->numberBetween(0, 10);
+            $newTrain->In_orario = $faker->time();
+            $newTrain->Cancellato = $faker->boolean();
+            $newTrain->save();
+        }
+    }
+}
